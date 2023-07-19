3cef5979b232137eeccffd85285be1a80e3911c8
coading-pooja <poojagoyal5106@gmail.com>
2023-07-19 21:20:27 +0530

    creat server

--- first-app.js
+++ first-app.js
@@ -1,3 +1,11 @@
-console.log("hello world")
-const fs = require('fs')
-fs.writeFileSync('hello.txt', 'hello from node js')
+console.log("hello world");
+const fs = require('fs');
+fs.writeFileSync('hello.txt', 'hello from node js');
+
+
+
+
+
+
+  
+
